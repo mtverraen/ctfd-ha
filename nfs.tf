505bc491a235077111f1a4a810498e771d0ce762
Martin Tverråen <martin.tverraen@evry.com>
2021-02-15 13:05:42 +0100

    set storageClass in ctfd config

--- nfs.tf
+++ nfs.tf
@@ -2,7 +2,7 @@ module "airflow_nfs" {
   source  = "kiwicom/gke-nfs/kubernetes"
   version = "0.1.0"
 
-  name      = "airflow-nfs"
+  name      = "airflow"
   namespace = kubernetes_namespace.ctfd.metadata.0.name
   zones     = ["europe-west1-b", "europe-west1-c"]
 
